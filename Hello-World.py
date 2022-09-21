3edd2df8b475632c000b50b758e031c8b31ed572
JingShing <JingShingLai@gmail.com>
2022-09-21 21:11:36 +0800

    Create Hello-World.py

---
+++ Hello-World.py
@@ -0,0 +1,12 @@
+# normal person
+print("Hello World!")
+
+# Crazy
+import sys
+Class Hello_World:
+  def Hello_World():
+    Hello_World='Hello World!'
+    print(Hello_World,end='')
+    sys.exit(0)
+Hello_World = Hello_World()
+Hello_World.Hello_World()
